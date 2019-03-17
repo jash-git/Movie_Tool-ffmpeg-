d1b8d5c7846ab47cebac1e88cbb81696ed9c5c83
jash.liao <jash.liao@gmail.com>
2019-03-17 21:52:23 +0800

    modified 2019/03/17

--- CutMp4.bat
+++ CutMp4.bat
@@ -1,1 +1,3 @@
-ffmpeg -ss 00:06:00 -t 00:06:17 -i "C:\360p.mp4" -vcodec copy -acodec copy  "C:\output.mp4"
+ffmpeg -ss 00:00:40 -t 00:00:42 -i "videoplayback.mp4" -vcodec copy -acodec copy  "01.mp4"
+ffmpeg -ss 00:01:23 -t 00:00:45 -i "videoplayback.mp4" -vcodec copy -acodec copy  "02.mp4"
+ffmpeg -ss 00:02:09 -t 00:01:03 -i "videoplayback.mp4" -vcodec copy -acodec copy  "03.mp4"
--- Readme.txt
+++ Readme.txt
@@ -1,3 +1,7 @@
 ﻿CutMp4.bat-單純影片切割
+	凱格爾(防止漏尿)影片分段切割 - https://www.youtube.com/watch?v=Ml4pL0-kkOs
+		00:40~01:21
+		01:23~02:08
+		02:09~03:12	
 Any2MP4.bat-單純影片轉檔
 Any2mp3.bat-單純聲音轉換
